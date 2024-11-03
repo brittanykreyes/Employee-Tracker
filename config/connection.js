0ae9c03e7ee150fc21849fda68cc83afe39b7ec6
BrittanyReyes <brittanyk.reyes@gmail.com>
2024-11-03 13:48:36 -0500

    edited schema

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     host: 'localhost',
-    user: 'your_username',  // Replace with your PostgreSQL username
-    password: 'your_password',  // Replace with your PostgreSQL password
+    user: 'postgres',
+    password: 'kaylinjae14!!!',  // Replace with your PostgreSQL password
     database: 'employee_db',  // Replace with your database name
     port: 5432,
 });
